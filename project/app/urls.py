7199bdbf11d8bd1403c753bc12786c433231ce5f
Arthur Kwabena Larbi Nathaniel <105647591+ArthurNathaniel@users.noreply.github.com>
2023-04-23 01:34:30 -0500

    changes

--- project/app/urls.py
+++ project/app/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name = 'index'),
-     path('insert', views.insertData, name = 'insertData'),
+    path('', views.index, name='index'),
+    path('insert', views.insertData, name='insertData'),
+    path('update/<id>', views.insertData, name='insertData'),
+    path('delete/<id>', views.insertData, name='insertData'),
 ]
